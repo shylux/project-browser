c20b6be79679b962ac32d6af61087e9f6404808e
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-03-29 14:13:52 +0000

    Defined Headers and code skeleton

---
+++ trunk/HirarchicalView.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+#File:			HirarchicalView.py
+#Description:		Klasse um die Orderstrucktur anzuzeigen
+#Author:		Kaleb Tschabold
+#Creation Date:		29.3.2011
+#
+#History: 		--Version--	--Date--	--Activities--
+#			0.1		29.3.2011	Grundfunktionalitaeten werden erstellt
+
+class HirarchicalView(View):
+	def __init__(self):
+		pass
