80c9e5815ef578c79a20f15e8fa8f37d996e8faa
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-14 14:52:36 +0000

    moved all programm-files in the exmerimental order

---
+++ trunk/experimental/GUI.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#File:			GUI.py
+#Description:		Klasse die die Grafikanzeige verwaltet
+#Author:		Kaleb Tschabold
+#Creation Date:		29.3.2011
+#
+#History: 		--Version--	--Date--	--Activities--
+#			0.1		29.3.2011	Grundfunktionalitaeten werden erstellt
+
+from HirarchicalView import *
+from TagView import *
+
+class GUI():
+	def __init__(self):
+		pass
