76d39482a4a5b9adeb91070564227f712aee470c
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-18 17:40:15 +0000

    Add a bit more event listeners

--- experimental/small-test-programms/filesystem/filesyswatch_linux_event.py
+++ experimental/small-test-programms/filesystem/filesyswatch_linux_event.py
@@ -2,7 +2,7 @@
 import pyinotify
 print "Hy"
 wm = pyinotify.WatchManager()  # Watch Manager
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY # watched events
+mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_DELETE_SELF | pyinotify.IN_MOVE_SELF #  watched events
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
@@ -14,9 +14,15 @@ class EventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
 	print "Modifying:", event.pathname
 
+    def process_IN_DELETE_SELF(self,event):
+        print "Deleting self:",event.pathname
+
+    def process_IN_MOVE_SELF(self,event):
+        print "Moving self:",event.pathname
+
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('/home/shylux/project-browser', mask, rec=True)
+wdd = wm.add_watch('/home/vmadmin/Downloads', mask, rec=True)
 
 print "start loop"
 notifier.loop()
