f45027041805c61b76faa3df5e0f4c6a24a9ddce
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-18 17:34:22 +0000

    inherit from view and run __init__ from view

--- trunk/experimental/HirarchicalView.py
+++ trunk/experimental/HirarchicalView.py
@@ -7,9 +7,12 @@
 #
 #History: 		--Version--	--Date--	--Activities--
 #			0.1		29.3.2011	Grundfunktionalitaeten werden erstellt
+#			0.1		18.3.2011	Erben von View und init Funktion aufrufen
 
 from View import *
+import gtk
 
 class HirarchicalView(View):
 	def __init__(self):
+		View.__init__(self)
 		pass
