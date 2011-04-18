3e21c93c8186e69302e2355a4ff0251c5865f74c
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-18 17:39:33 +0000

    Class for Constants

---
+++ trunk/experimental/Constante.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#File:			Constante.py
+#Description:		Hier sind alle Wichtigen Konstanten fuers Programm an einer zentrallen Stelle gespeichert.
+#Author:		Kaleb Tschabold
+#Creation Date:		16.4.2011
+#
+#History: 		--Version--	--Date--	--Activities--
+#			0.1		16.4.2011	Grundfunktionalitaeten werden erstellt
+
+class Constante():
+	def __init__(self):
+		#time to sleep in a endlos loop
+		self.sleep = 0.1
