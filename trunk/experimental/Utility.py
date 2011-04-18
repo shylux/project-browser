5fe01b54142d175d816de974c3ec7a88090d6ce4
tschabold.kaleb@gmail.com <tschabold.kaleb@gmail.com@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-18 17:37:11 +0000

    New Function: NO() -> Create a new object

--- trunk/experimental/Utility.py
+++ trunk/experimental/Utility.py
@@ -7,12 +7,21 @@
 #
 #History: 		--Version--	--Date--	--Activities--
 #			0.1		14.4.2011	Grundfunktionalitaeten werden erstellt
+#			0.2		18.4.2011	Funktion um ein neues Objekt zu erstellen
+
 import sys
 
 class Utility:
+
 	def __init__(self):
+		self.NO = NO
 		pass
+
 	def checkOS(self):
 		platform = sys.platform
 		if platform.startswith("linux"):
 			 return "linux"
+
+#Dynamisches Objekt, dass fuer normale Objekte gebraucht werden kann
+class NO():
+	pass
