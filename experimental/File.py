1b222fddbd08d2296ed3e6962f948c26d21f8f89
niklaus.hofer <niklaus.hofer@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-04-23 21:01:23 +0000

    DB.py has now an addFile() method that takes files and adds them to the database (including all tags 'n' stuff...). ATTENTION! I changed the database-scheme in the creation script! The old design was rubish. Please delete your old database file, a new, correct one will then automatically be created

--- experimental/File.py
+++ experimental/File.py
@@ -16,7 +16,7 @@ class File:
 	path		= None
 	tags		= None
 	backup		= None
-	def __init__(self, fileName=None, path=None, tags=None, backup=None):
+	def __init__(self, fileName=None, path=None, tags=[], backup='false'):
 	  	"""Constructor
 		@param	fileName, string	, optional 
 		@param	path	, string	, optional
