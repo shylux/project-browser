cbdc66ab8af088e80e2f1d4d78aee798c8b2ca25
niklaus.hofer <niklaus.hofer@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-05-04 17:23:55 +0000

    cleaned up some stuff in DB.py (including deleting old tags after updateFile()), addTag is now deprecated (see the coment in the code to see why). Added getter and setter for fullPath in File.py and added some more tests

--- trunk/experimental/File.py
+++ trunk/experimental/File.py
@@ -39,18 +39,18 @@ class File:
 				match = p.match(fullPath)
 				if not match.group(1) == None and not match.group(2) == None:
 					self.path = match.group(1)
-					print self.path
 					self.fileName = match.group(2)+match.group(3)
-					print self.fileName
 			else:
 				p = re.compile("((?:.*\\\)|(?:.*/))((?:[^/]*)|(?:[^\\\]*))")
 				match = p.match(fullPath)
 				if not match.group(1) == None and not match.group(2) == None:
 					self.path = match.group(1)
-					print self.path
 					self.fileName = match.group(2)
-					print self.fileName
 
+	def __checkPaths(self):
+		# Check wether all paths have / (or \ on Windows) at the end
+		#TODO implement
+		pass
 	
 	def setFileName(self, fileName):
 	  	"""@param	filename	, string"""
@@ -121,46 +121,94 @@ if __name__ == "__main__":
 	print "=============="
 	x = File()
 	if x.getFileName() == None:
-		print "Test #1: Succeed"
+		print "Test #01: Succeed"
 	else:
-		print "Test #1: FAIL"
+		print "Test #01: FAIL"
 
 	x.setFileName("test")
 	if x.getFileName() == "test":
-		print "Test #2: Succeed"
+		print "Test #02: Succeed"
 	else:
-		print "Test #2: FAIL"
+		print "Test #02: FAIL"
 
 	x.setTags(["test", "tag"])
 	if x.getTags() == ["test","tag"]:
-		print "Test #3: Succeed"
+		print "Test #03: Succeed"
 	else:
-	  	print "Test #3: FAIL"
+	  	print "Test #03: FAIL"
 
 	x.addTag("new tag")
 	if x.getTags()[2] == "new tag":
-		print "Test #4: Succeed"
+		print "Test #04: Succeed"
 	else:
-	  	print "Test #4: FAIL"
+	  	print "Test #04: FAIL"
 
 	x.addTags(["extend","linux","unix"])
 	if x.getTags()[3:] == ["extend","linux","unix"]:
-		print "Test #5: Succeed"
+		print "Test #05: Succeed"
 	else:
-	  	print "Test #5: FAIL"
+	  	print "Test #05: FAIL"
+	
+	x.setIsDir(True)
+	if x.getIsDir():
+		print "Test #06: Succeed"
+	else:
+	  	print "Test #06: FAIL"
+
+	#Testing the path stuff...
+	f1 = File(fullPath="/home/niklaus/lol.txt")
+	if f1.getPath() == "/home/niklaus/" \
+	    and f1.getFileName() == "lol.txt" and f1.getFullPath() == "/home/niklaus/lol.txt":
+		print "Test #07: Succeed"
+	else:
+		print "Test #07: FAIL"
+
+	f2 = File(fullPath="C:\\windows\\system32\\chlous.txt.test")
+	if f2.getPath() == "C:\\windows\\system32\\" \
+	    and f2.getFileName() == "chlous.txt.test" and f2.getFullPath() == "C:\\windows\\system32\\chlous.txt.test":
+		print "Test #08: Succeed"
+	else:
+	  	print "Test #09: FAIL"
+
+	f3 = File(fullPath="C:\\windows\\system32\\")
+	if f3.getPath() == "C:\\windows\\" \
+	    and f3.getFileName() == "system32\\" and f3.getFullPath() == "C:\\windows\\system32\\":
+		print "Test #10: Succeed"
+	else:
+	  	print "Test #10: FAIL"
+	f4 = File(fullPath="/home/niklaus/Music/")
+	if f4.getPath() == "/home/niklaus/" \
+	    and f4.getFileName() == "Music/" and f4.getFullPath() == "/home/niklaus/Music/":
+		print "Test #11: Succeed"
+	else:
+	  	print "Test #11: FAIL"
+
+	f5 = File(fileName="document.odt", path="/home/niklaus/Documents/")
+	if f5.getFullPath() == "/home/niklaus/Documents/document.odt":
+		print "Test #12: Succeed"
+	else:
+		print "Test #12: FAIL"
+
+	f6 = File(fileName="Videos/", path="/home/niklaus/", isDir=True)
+	if f6.getFullPath() == "/home/niklaus/Videos/" and f6.getIsDir() == True:
+		print "Test #13: Succeed"
+	else:
+	  	print "Test #13: FAIL"
+
+	f7 = File(fileName="hosts", path="C:\\windows\\system32\\etc\\")
+	if f7.getFullPath() == "C:\\windows\\system32\\etc\\hosts":
+		print "Test #14: Succeed"
+	else:
+		print "Test #14: FAIL"
+
+	f8 = File(fileName="etc\\", path="C:\\windows\\system32\\", isDir=True)
+	if f8.getFullPath() == "C:\\windows\\system32\\etc\\" and f8.getIsDir() == True:
+		print "Test #15: Succeed"
+	else:
+	  	print "Test #15: FAIL"
+
+	print File.__init__.__doc__
 
 	#Backup
 	b = File()
 	b.ex_backup()
-	
-	x.setIsDir(True)
-	if x.getIsDir():
-		print "Test #6: Succeed"
-	else:
-	  	print "Test #6: FAIL"
-
-	test = File(fullPath="/home/niklaus/lol.txt")
-	test2 = File(fullPath="C:\\windows\\system32\\chlous.txt.test")
-	test3 = File(fullPath="C:\\windows\\system32\\")
-	test4 = File(fullPath="/home/niklaus/Music/")
-	print File.__init__.__doc__
