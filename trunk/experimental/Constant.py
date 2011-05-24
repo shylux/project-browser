8bb03f318ba775ecfe50678467d64ddc96ff7477
shylux <shylux@b466466e-c1c7-ebde-a40c-ca58ec8526be>
2011-05-24 08:44:41 +0000

    Watch home of user by default.

--- trunk/experimental/Constant.py
+++ trunk/experimental/Constant.py
@@ -14,12 +14,14 @@ class Constant():
 	prgname	= None
 	version	= None
 	dbPath	= None
+	home	= None
 	def __init__(self,sys):
 		#time to sleep in a endlos loop
 		self.sleep	= 0.1
 		self.prgname	= "Project Browser"
 		self.version	= 0.8
 		self.dbPath	= os.path.expanduser("~/.project-browser/")
+		self.home	= os.path.expanduser("~")
 		#self.startview = 'tag'
 		self.startview = 'hirarchical'
 		self.os = sys.u.checkOS()
